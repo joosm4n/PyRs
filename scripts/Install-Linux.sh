a9fb6b9c2874d30bc92ae5917923068fc0283e9d
joosm4n <kotoole0703@gmail.com>
2026-01-23 23:31:52 +1100

    added linux install script

---
+++ scripts/Install-Linux.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+cd ../
+
+if command -v cargo &> /dev/null
+then
+    cargo --version
+    echo "Cargo is installed. Proceeding with script..."
+
+else
+    echo "Cargo is not installed. Please install Rust and Cargo."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    rustup toolchain install stable
+
+if command -v gcc &> /dev/null
+then
+    echo "GCC C compiler is installed."
+    echo "Version information:"
+    gcc --version
+else
+    sudo apt update
+    sudo apt install build-essential
+    sudo apt install libgmp-dev libmpfr-dev
+
+if command -v m4 &> /dev/null
+then 
+    echo "m4 already instlled"
+else
+    sudo apt install m4
+
+cargo build
+
+
