566f8fd6a016052e42eb0e0e937ee4adf5ba49c9
joosm4n <kotoole0703@gmail.com>
2026-05-13 19:39:07 +1000

    rework of structure

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,4 @@
-pub mod pyrs_bytecode;
-pub mod pyrs_codeobject;
-pub mod pyrs_error;
-pub mod pyrs_interpreter;
-pub mod pyrs_modules;
-pub mod pyrs_obj;
-pub mod pyrs_parsing;
-pub mod pyrs_pyc;
-pub mod pyrs_pyobject;
-pub mod pyrs_serializer;
-pub mod pyrs_std;
-mod pyrs_tests;
-pub mod pyrs_utils;
-pub mod pyrs_vm;
+use pyrs::*;
 
 #[allow(unused_imports)]
 use crate::{
